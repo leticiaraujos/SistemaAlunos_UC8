4f9b32d4ab01ae8bd8023232fc3038ab8daf0096
Leticia Araujo <lettaraujos@hotmail.com>
2022-08-10 19:20:31 -0300

    first commit

---
+++ SistemaContagem.js
@@ -0,0 +1,15 @@
+//Número Par, escrever 'par' e o número correspondente 
+//Número ímpar, escrever 'ímpar' e o número correspondente
+//Se o número for zero, escrever zero 
+
+var numeroAlunos = 10;
+for (var numeroAlunos = 0, contador < numeroAlunos.length; contador ++) {
+    if ( contador  ==  0 ) {
+        consola . log ( "O número atual é zero" )}
+    else if (contador % 2 == 0){
+        consola.log("O número é par")
+    } senão {
+        consola.log(" O número é ímpar")
+    }
+}
+
